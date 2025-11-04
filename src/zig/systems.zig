5907a8a979dbaa384abe490cbf1ac78cd9ae36ca
Vince Carrillo <nfgauldun@outlook.com>
2025-11-04 09:08:46 -0700

    Notes

--- src/zig/systems.zig
+++ src/zig/systems.zig
@@ -4,3 +4,12 @@ const stdout = std.fs.File.stdout();
 const CPP = @cImport({
     @cInclude("c_wrapper.hpp");
 });
+
+pub fn welcomeMessage() void {
+    stdout.writeAll("Welcome to [GAME_NAME]!\n ");
+    // Basic game play explanation:
+    // Keybindings explanation:
+    // General goal explanation:
+}
+
+
