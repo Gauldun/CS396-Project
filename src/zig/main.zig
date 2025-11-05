794f3715e4c69050f63514bc5de93bdc1aa5789a
Vince Carrillo <nfgauldun@outlook.com>
2025-11-05 16:43:57 -0700

    Updates

--- src/zig/main.zig
+++ src/zig/main.zig
@@ -9,28 +9,57 @@ const CPP = @cImport({
 const createPlayerChar = CPP.PlayerEntityCreate;
 const destroyPlayerChar = CPP.PlayerEntityDestroy;
 
+// Enemy Character CPP function const calls
+const createEnemyChar = CPP.EnemyEntityCreate;
+const destroyEnemyChar = CPP.EnemyEntityDestroy;
+
 // Standard Library const function calls
-const printErr = std.debug.print;
+const stderr = std.debug.print;
 const stdout = std.fs.File.stdout();
 const printBuffer = std.fmt.bufPrint;
 
+// Systems.zig Library const function calls
+const stdin = sys.readInput;
 // Global buffer for printing game logic 
 var BUFF: [4096]u8 = undefined; // Size is 4 Kilobytes 
 
 pub fn main() !void {
-    try sys.welcomeMessage();
-    // Tank Player Party Character w/ Base Stats; 100 Health, 15 Damage, 1 Range, 3 Speed
-    const tankChar = createPlayerChar(100, 15, 1, 3);
-    // Priest Player Party Character w/ Base Stats; 75 Health, 0 Damage, 3 Range, 5 Speed
-    const priestChar = createPlayerChar(75, 0, 3, 5);
-    // Archer Player Party Character w/ Base Stats; 50 Health, 30 Damge, 3 Range, 7 Speed
-    const archerChar = createPlayerChar(50, 30, 3, 7);
-
-    // GENERAL GAME LOOP HERE
-
-    // Call Destructors
-    destroyPlayerChar(tankChar);
-    destroyPlayerChar(priestChar);
-    destroyPlayerChar(archerChar);
+    // try sys.welcomeMessage();
+    // // Tank Player Party Character w/ Base Stats; 100 Health, 15 Damage, 1 Range, 3 Speed
+    // const tankChar = createPlayerChar(100, 15, 1, 3);
+    // // Priest Player Party Character w/ Base Stats; 75 Health, 0 Damage, 3 Range, 5 Speed
+    // const priestChar = createPlayerChar(75, 0, 3, 5);
+    // // Archer Player Party Character w/ Base Stats; 50 Health, 30 Damge, 3 Range, 7 Speed
+    // const archerChar = createPlayerChar(50, 30, 3, 7);
+    //
+    // //Enemy Character
+    // const enemyGrunt = createEnemyChar(100, 50, 2, 3);
+    //
+    // const message1 = try printBuffer(&BUFF, "Health: {}\n", .{CPP.PlayerEntityGetHealth(tankChar)});
+    // try stdout.writeAll(message1);
+    //
+    // sys.applyPlayerDamage(tankChar orelse {return;}, 25, 100);
+    // const message2 = try printBuffer(&BUFF, "Health: {}\n", .{CPP.PlayerEntityGetHealth(tankChar)});
+    // try stdout.writeAll(message2);
+    //
+    // const message3 = try printBuffer(&BUFF, "Health: {}\n", .{CPP.EnemyEntityGetHealth(enemyGrunt)});
+    // try stdout.writeAll(message3);
+    // sys.applyPlayerDamage(enemyGrunt orelse {return;}, 25, 100);
+    // const message4 = try printBuffer(&BUFF, "Health: {}\n", .{CPP.EnemyEntityGetHealth(enemyGrunt)});
+    // try stdout.writeAll(message4);
+    //
+    //
+    // // Call Destructors
+    // destroyPlayerChar(tankChar);
+    // destroyPlayerChar(priestChar);
+    // destroyPlayerChar(archerChar);
+    // destroyEnemyChar(enemyGrunt);
+    //
+    //General Game loop
+    // while (true) {
+    //     // Prompt User for choice of attack
+    //     // Prompt requires choice of player character to attack with, which enemy to attack (currently there are no abilities [Simple Point & Click])
+    //     stdout.WriteAll("Choose which player character to attack with (Rear[3], Middle[2], Front[1]\n");
+    // }
 }
 
--- src/zig/systems.zig
+++ src/zig/systems.zig
@@ -1,9 +1,16 @@
+//Library const imports
 const std = @import("std");
-const printErr = std.debug.print;
-const stdout = std.fs.File.stdout();
 const CPP = @cImport({
     @cInclude("c_wrapper.hpp");
 });
+
+// Standard lib function const calls
+const printErr = std.debug.print;
+const stdout = std.fs.File.stdout();
+
+// CPP Const function calls
+const setPlayerHealth = CPP.PlayerEntitySetHealth;
+const setEnemyHealth = CPP.EnemyEntitySetHealth;
 
 pub fn welcomeMessage() !void {
     try stdout.writeAll("Welcome to [GAME_NAME]!\n ");
@@ -12,4 +19,41 @@ pub fn welcomeMessage() !void {
     // General goal explanation:
 }
 
+pub fn readInput() ![]const u8 {
+    // 1. Get the stdout writer and stdin reader
+    var stdin_buffer: [63]u8 = undefined;
+    var stdin_reader = std.fs.File.stdin().reader(&stdin_buffer);
+    const stdin = &stdin_reader.interface;
 
+    const line = try stdin.takeDelimiterExclusive('\n');
+
+    return line;
+}
+// Functional: Returns new health value after character takes damage
+pub fn calcDamage(damage: i32, health: i32) i32 {
+    const result = if ((health - damage) <= 0) 0 else health - damage;
+    return result;
+} 
+
+// Functional: Returns new modified value after character acquires some modifier (Could be an item or game effect)
+pub fn calcModifiedVal(modifier: i32, currVal: i32) i32 {
+    if (currVal == 0) {
+        return currVal;
+    }
+    else {
+    const result = if ((currVal * modifier) <= 1) 1 else currVal * modifier;
+    return result;
+    }
+}
+
+// Imperative: Applies resulting damage onto player character
+pub fn applyPlayerDamage(player: *const CPP.PlayerEntityHandle, damage: i32, health: i32) void {
+    const result = calcDamage(damage, health);
+    setPlayerHealth(@constCast(player), result);
+}
+
+// Imperative: Applies resulting damage onto player character
+pub fn applyEnemyDamage(enemy: *const CPP.EnemyEntityHandle, damage: i32, health: i32) void {
+    const result = calcDamage(damage, health);
+    setEnemyHealth(@constCast(enemy), result);
+}
